28ce4cb53fcd9f0b0f3667f8be4f0eb502db1e4e
mohamed keshawy <mokeshawy@gmail.com>
2024-05-25 18:27:25 +0300

    create YajhazApplication and setup hilt of dependency injection

---
+++ app/src/main/java/com/mycash/yajhaz/yajhaz_applecation/YajhazApplication.kt
@@ -0,0 +1,8 @@
+package com.mycash.yajhaz.yajhaz_applecation
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class YajhazApplication : Application() {
+}
