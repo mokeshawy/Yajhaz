f69cf88d10c74008d30edcb547ad6da4f8860dcf
mohamed keshawy <mokeshawy@gmail.com>
2024-05-26 07:40:08 +0300

    implement design of login screen

--- app/src/main/java/com/mycash/yajhaz/features/fragment/login/presentation/LoginFragment.kt
+++ app/src/main/java/com/mycash/yajhaz/features/fragment/login/presentation/LoginFragment.kt
@@ -1,4 +1,15 @@
 package com.mycash.yajhaz.features.fragment.login.presentation
 
-class LoginFragment {
+import android.os.Bundle
+import android.view.View
+import com.mycash.yajhaz.core.base_fragment.BaseFragment
+import com.mycash.yajhaz.databinding.FragmentLoginBinding
+
+class LoginFragment : BaseFragment<FragmentLoginBinding>() {
+
+    override val binding by lazy { FragmentLoginBinding.inflate(layoutInflater) }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
 }
