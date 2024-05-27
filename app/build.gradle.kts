052c61a4e05f74cd3b345a7c0ec12721077e8f6c
mohamed keshawy <mokeshawy@gmail.com>
2024-05-27 21:32:35 +0300

    update build version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.mycash.yajhaz"
         minSdk = 24
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.2.0"
+        versionCode = 3
+        versionName = "1.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
